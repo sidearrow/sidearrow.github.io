200ad140439253246fae1caa4e729fc5e7bdf1b8
sidearrow <sidearrow@users.noreply.github.com>
2021-07-11 14:09:20 +0000

    deploy: cd84bf928123a76f5c8d0bac6883b78bb452e9c2

--- _next/static/y52qF9DDSwcj78AgWlWvT/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/404":["static\u002Fchunks\u002Fpages\u002F404-162e1856fe1da175b379.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-986b9ac52257fbcb5ebd.js"],sortedPages:["\u002F404","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
